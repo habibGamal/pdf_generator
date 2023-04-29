cc358572cdd35a0d7e0f5cb2b852551dbb4551cb
unknown <habibmisi3@gmail.com>
2023-04-29 18:59:51 +0200

    final

---
+++ src/data.js
@@ -0,0 +1,282 @@
+const data = [
+    {
+        "Chest & Back": [
+            {
+                name: "Barbell Bench press",
+                link: "https://youtu.be/tuwHzzPdaGc",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: "decline dumbbell bench press",
+                link: "https://youtu.be/5pDbtYmX5fM",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: "incline dumbbell press",
+                link: "https://youtu.be/8nNi8jbbUPE",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: "Machine fly",
+                link: "https://youtu.be/Z57CtFmRMxA",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: "bent over dumbbell row",
+                link: "https://youtu.be/PgpQ4-jHiq4",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: "bent over barbell row",
+                link: "https://youtu.be/paCfxhgW6bI",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: undefined,
+                link: "https://youtu.be/5oxviYmdHCY",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: undefined,
+                link: "https://youtu.be/uAyrz5GTEHg",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: undefined,
+                link: "https://youtu.be/1ZXobu7JvvE",
+                sets: "4 sets 12,10,8,8",
+            },
+        ],
+    },
+    {
+        "Shoulder & triceps & biceps": [
+            {
+                name: "Standing Dumbbell Press",
+                link: "https://www.menshealth.com/uk/fitness/a735489/dumbbell-standing-shoulder-press/",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: undefined,
+                link: "https://www.muscleandstrength.com/exercises/dumbbell-lateral-raise.html",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: "dumbbell front raise",
+                link: "https://youtu.be/-t7fuZ0KhDA",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: "barbell upright row",
+                link: "https://youtu.be/MThM64GU_Zo",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: "bent over dumbbell fly",
+                link: "https://youtu.be/Fgz_FdzDukE",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: "triceps v bar press down",
+                link: "https://youtu.be/nNX-UJjatF0",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: "lying triceps extension",
+                link: "https://www.muscleandstrength.com/exercises/lying-tricep-extension.html",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: undefined,
+                link: "https://youtu.be/mRozZKkGIfg",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: undefined,
+                link: "https://youtu.be/ke2shAeQ0O8",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: "Incline Dumbbell Curl",
+                link: "https://youtu.be/UeleXjsE-98",
+                sets: "4 sets 12,10,8,8",
+            },
+        ],
+    },
+    {
+        "Leg 🦵": [
+            {
+                name: undefined,
+                link: "https://youtu.be/Otsrtdyw2fQ",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: undefined,
+                link: "https://vimeo.com/281064077",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: undefined,
+                link: "https://youtu.be/0tn5K9NlCfo",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: undefined,
+                link: "https://youtu.be/0fl1RRgJ83I",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: undefined,
+                link: "https://youtu.be/1Tq3QdYUuHs",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: undefined,
+                link: "https://youtu.be/29OfN4ztW_g",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: undefined,
+                link: "https://youtu.be/GAQ-oohMhog",
+                sets: "3 sets nutral 3 sets 7 shape",
+            },
+        ],
+    },
+    {
+        "Chest &Back": [
+            {
+                name: "barbell incline bench press",
+                link: "https://youtu.be/uIzbJX5EVIY",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: "Leverage Decline press",
+                link: "https://youtu.be/xK9zpXvjFUg",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: "Hummer chest press",
+                link: "https://youtu.be/Hw0vnL2B8Uc",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: "Push up",
+                link: "https://youtu.be/ZWdBqFLNljc",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: "Lever Reverse T-Bar Row",
+                link: "https://www.mybodycreator.com/exercises/lever-reverse-t-bar-row",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: "chest supported dumbbell row",
+                link: "https://youtu.be/oKNjFM1bxAs",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: "chin ups",
+                link: "https://youtu.be/T78xCiw_R6g",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: "seated wide grip cable row",
+                link: "https://youtu.be/iXGHN-MWFfY",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: "Dumbbell shrug",
+                link: "https://youtu.be/dj2Gm628kas",
+                sets: "4 sets 12,10,8,8",
+            },
+        ],
+    },
+    {
+        "Shoulders & triceps & biceps": [
+            {
+                name: "Rear Delt Cable Flys",
+                link: "https://youtu.be/f5V2XHVR3Dg",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: "machine flyes rear delts",
+                link: "https://youtu.be/VoscK_FHNbw",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: "lever shoulder press",
+                link: "https://youtu.be/qeHtqtNtngo",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: "cable lateral raise",
+                link: "https://youtu.be/Fv-eAW1uKDI",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: "cable front delt raise",
+                link: "https://youtu.be/wxzEul89zR0",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: "reverse curl ez bar",
+                link: "https://www.muscleandstrength.com/exercises/ez-bar-reverse-grip-barbell-curl.html",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: "preacher curl",
+                link: "https://youtu.be/RgN216Cumtw",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: "alternating hammer curls",
+                link: "https://youtu.be/cYOzpQmfLUA",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: "tricep kickbacks",
+                link: "https://youtu.be/6SS6K3lAwZ8",
+                sets: "4 sets 12,10,8,8",
+            },
+            {
+                name: "triceps v bar press down",
+                link: "https://youtu.be/nNX-UJjatF0",
+                sets: "4 sets 12,10,8,8",
+            },
+        ],
+    },
+    {
+        "Leg 🦵": [
+            {
+                name: undefined,
+                link: "https://youtu.be/R2dMsNhN3DE",
+                sets: "5 sets 15,15,12,12,10",
+            },
+            {
+                name: undefined,
+                link: "https://youtu.be/uRSsOoZG9z8",
+                sets: "5 sets 15,15,12,12,10",
+            },
+            {
+                name: undefined,
+                link: "https://youtu.be/Orxowest56U",
+                sets: "5 sets 15,15,12,12,10",
+            },
+            {
+                name: undefined,
+                link: "https://youtu.be/5Y3KW5rWMh4",
+                sets: "4 sets 20,18,15,12",
+            },
+            {
+                name: undefined,
+                link: "https://youtu.be/1ZXobu7JvvE",
+                sets: "4 sets 20,18,15,12",
+            },
+            {
+                name: undefined,
+                link: "https://youtu.be/IrxfRHGiGE0",
+                sets: "4 sets 20,18,15,12",
+            },
+        ],
+    },
+];
+export default data;
